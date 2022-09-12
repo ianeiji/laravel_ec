c66c5a5147bf8672b1f16c79864314d467723466
ianeiji <tajima830@gmail.com>
2022-09-13 08:05:59 +0900

    props

--- resources/views/test/component-test1.blade.php
+++ resources/views/test/component-test1.blade.php
@@ -1,3 +1,5 @@
+
+
 <x-tests.app>
 
     <x-slot name="header">ヘッダー1</x-slot>
@@ -5,4 +7,5 @@
 コンポーネントテスト1
 
 <x-tests.card title="タイトル" content="本文" :message="$message"/>
+<x-tests.card title="タイトル"/>
 </x-tests.app>
