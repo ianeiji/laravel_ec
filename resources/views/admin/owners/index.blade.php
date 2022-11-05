b2482e8243f040e05ec88e60964bfa00f645ec72
ianeiji <tajima830@gmail.com>
2022-11-05 18:47:46 +0900

    image delete

--- resources/views/admin/owners/index.blade.php
+++ resources/views/admin/owners/index.blade.php
@@ -38,7 +38,7 @@
                                 <form id="delete_{{ $owner->id }}" method="post" action="{{ route('admin.owners.destroy',['owner' => $owner->id]) }}">
                                 @csrf
                                 @method('delete')
-                                    <td class="px-4 py-3">
+                                    <td class="md:px-4 py-3">
                                         <a href="#" data-id="{{ $owner->id }}" onclick="deletePost(this)" class="text-white bg-red-400 border-0 py-2 px-4 focus:outline-none hover:bg-red-500 rounded">削除</a>
                                     </td>
                                 </form>
